9b6e989eef0787835a48592a8a819249999e743b
缺心眼 <yuwei_shiny@163.com>
2016-06-05 20:33:40 +0800

    解决连续点击出现空白的bug

--- js/app.js
+++ js/app.js
@@ -54,31 +54,31 @@ var roll = {
         })
 
     },
-    list:function(){
-        var list=$('.product-list'),next=$('.list-next'),prev=$('.list-prev'),listTimer=null;
-        var liLength=list.children().length;
-        var liWidth=list.children().outerWidth(true);
-        var num=5;
+    list: function () {
+        var list = $('.product-list'), next = $('.list-next'), prev = $('.list-prev'), listTimer = null;
+        var liLength = list.children().length;
+        var liWidth = list.children().outerWidth(true);
+        var num = 5;
         insert();
-        var Left=-liWidth*num;  //初始偏移位置
-        list.css({'width':liWidth*list.children().length,'left':Left});
+        var Left = -liWidth * num;  //初始偏移位置
+        list.css({'width': liWidth * list.children().length, 'left': Left});
 
         //在原始图片前后各追加5个图片
-        function insert(){
-            for(var i=0;i<2;i++){
-                var arr=[];
-                for(var j=0;j<num;j++){
-                    if(!i){
+        function insert() {
+            for (var i = 0; i < 2; i++) {
+                var arr = [];
+                for (var j = 0; j < num; j++) {
+                    if (!i) {
                         //第一次
                         arr.push(list.children().eq(j).get(0).outerHTML);    //获得第N个子元素的html结构,正向取
-                    }else{
+                    } else {
                         //第二次
-                        arr.push(list.children().eq(liLength-j-1).get(0).outerHTML);    //获得第N个子元素的html结构,反向取
+                        arr.push(list.children().eq(liLength - j - 1).get(0).outerHTML);    //获得第N个子元素的html结构,反向取
                     }
                 }
-                if(!i){
+                if (!i) {
                     list.append(arr);
-                }else{
+                } else {
                     list.prepend(arr.reverse());
                 }
             }
@@ -88,36 +88,42 @@ var roll = {
             next.trigger('click');
         }
 
-        next.on('click',function () {
-            if (list.children().length % liLength === 0) {
-                //倍数
-                Left-=liWidth*num;
-            } else {
-                //不是倍数
-                Left-=liWidth;
-            }
-            list.animate({'left':Left},500,function(){
-                if(Left===-(list.children().length-num)*liWidth){
-                    Left=-liWidth*num;
-                    list.css("left",Left);
+        next.on('click', function () {
+            if (!list.is(':animated')) {
+                if (list.children().length % liLength === 0) {
+                    //倍数
+                    Left -= liWidth * num;
+                } else {
+                    //不是倍数
+                    Left -= liWidth;
                 }
-            })
+
+                list.animate({'left': Left}, 500, function () {
+                    if (Left === -(list.children().length - num) * liWidth) {
+                        Left = -liWidth * num;
+                        list.css("left", Left);
+                    }
+                })
+            }
         });
 
-        prev.on('click',function () {
-            if (list.children().length % liLength === 0) {
-                //倍数
-                Left+=liWidth*num;
-            } else {
-                //不是倍数
-                Left+=liWidth;
-            }
-            list.animate({'left':Left},500,function(){
-                if(Left===0){
-                    Left=-liWidth*liLength;
-                    list.css("left",Left);
+        prev.on('click', function () {
+            if (!list.is(':animated')) {
+                if (list.children().length % liLength === 0) {
+                    //倍数
+                    Left += liWidth * num;
+                } else {
+                    //不是倍数
+                    Left += liWidth;
                 }
-            })
+
+                list.animate({'left': Left}, 500, function () {
+                    if (Left === 0) {
+                        Left = -liWidth * liLength;
+                        list.css("left", Left);
+                    }
+                })
+            }
         })
 
         clearInterval(listTimer);
